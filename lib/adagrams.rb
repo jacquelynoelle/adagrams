30d66639dcc6b2acd9b06ddb957e496d84ca6692
jackie <jacquelyn.cheng@me.com>
2018-08-16 16:54:47 -0700

    Added comments and cleaned up some spacing

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,8 +1,13 @@
 require 'pry'
 require 'csv'
 
+# WAVE 1
+# The draw_letters method starts with a fresh pool of tiles and returns
+# an array of 10 letters (the "hand").
 def draw_letters
-
+  # letter_pool is an array of hashes that represent available tiles in the game
+  # in each hash, keys represent the letter on the tile
+  # the value in each hash represents the number of tiles with that letter
   letter_pool = [
     {"A" => 9},
     {"B" => 2},
@@ -35,38 +40,44 @@ def draw_letters
   hand = []
   while (hand.length < 10) do
     pull = letter_pool[rand(26)]
-    if (pull.values.reduce > 0)
-      letter = pull.keys.reduce
-      hand << letter
-      pull[letter] -= 1
+    if (pull.values.reduce > 0) # if the selected letter has tiles in the pool
+      letter = pull.keys.reduce # fetch the letter
+      hand << letter # add the selected letter to the hand
+      pull[letter] -= 1 # decrement the tiles available for selected letter
     end
   end
-
   return hand
 end
 
+# WAVE 2
+# The uses_available_letters method accepts a word from the user and returns
+# true if that word uses only letters available in hand, false if not.
 def uses_available_letters?(input, letters_in_hand)
+  # Create a copy of the hand to use within this method so the original hand
+  # remains unaltered.
   new_letters_in_hand = letters_in_hand.map do |letter|
     letter
   end
 
+  # To account for duplicate letters, delete letters from the hand as they are
+  # matched with letters from user input.
   word = input.upcase.split("")
   word.each do |letter|
-
       if new_letters_in_hand.include?(letter)
         new_letters_in_hand.delete_at(new_letters_in_hand.index letter)
       else
-        return false
+        return false # returns false if any letter user entered isn't in hand
       end
   end
-  return true
-
+  return true # returns true only after all letters confirmed in hand
 end
 
+# WAVE 3
+# The score_word method calculates the score for word by summing up the score
+# of each tile.
 def score_word(word)
   score = 0
   word.upcase.split("").each do |letter|
-
     if "AEIOULNRST".include? letter
       score += 1
     elsif "DG".include? letter
@@ -89,7 +100,9 @@ def score_word(word)
   return score
 end
 
-
+# WAVE 4
+# The highest_score_from method identifies the highest scoring word played
+# by the user, using tie-breaking logic when necessary.
 def highest_score_from(words)
   highest = {
         word: "",
@@ -114,7 +127,11 @@ def highest_score_from(words)
   return highest
 end
 
+# WAVE 5
+# The is_in_english_dict? method checks to make sure the user entered a word
+# that is in the English dictionary by comparing against a csv list of words.
 def is_in_english_dict?(input)
+  # Map csv words to an array
   dictionary_words = CSV.open('assets/dictionary-english.csv', headers: true).map do |row|
    row["Word"]
   end
@@ -125,5 +142,4 @@ def is_in_english_dict?(input)
   else
     return false
   end
-
 end
